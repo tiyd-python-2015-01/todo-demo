3a0a938b085d7b7b5f6d2beb1df919196189ce21
Clinton Dreisbach <clinton@dreisbach.us>
2015-02-18 12:14:24 -0500

    Add WTForms

--- todo.py
+++ todo.py
@@ -1,8 +1,11 @@
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
+from flask_wtf import Form
+from wtforms import StringField
+from wtforms.validators import DataRequired
 
 DATABASE = '/tmp/todo.db'
 DEBUG = True
@@ -15,6 +18,10 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
+
+
+class TodoForm(Form):
+    text = StringField('text', validators=[DataRequired()])
 
 
 class Todo(db.Model):
@@ -33,17 +40,24 @@ class Todo(db.Model):
 def index():
     current_todos = Todo.query.filter(Todo.completed_at == None).all()
     completed_todos = Todo.query.filter(Todo.completed_at != None).all()
+    new_todo_form = TodoForm()
     return render_template("index.html",
+                           new_todo_form=new_todo_form,
                            todos=current_todos,
                            completed=completed_todos)
 
 
 @app.route("/add", methods=['POST'])
 def add_todo():
-    text = request.form['text']
-    todo = Todo(text)
-    db.session.add(todo)
-    db.session.commit()
+    form = TodoForm()
+    if form.validate_on_submit():
+        todo = Todo(form.text.data)
+        db.session.add(todo)
+        db.session.commit()
+        flash("Your todo was created.")
+    else:
+        flash("Your todo could not be created.")
+
     return redirect(url_for('index'))
 
 
