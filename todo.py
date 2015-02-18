04a843c91d3f535fe9dc5f9af4e0df8e1ae5363d
Clinton Dreisbach <clinton@dreisbach.us>
2015-02-18 11:59:45 -0500

    Added todo completion as opposed to deletion

--- todo.py
+++ todo.py
@@ -1,5 +1,8 @@
+from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.script import Manager
+from flask.ext.migrate import Migrate, MigrateCommand
 
 DATABASE = '/tmp/todo.db'
 DEBUG = True
@@ -9,11 +12,15 @@ SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE
 app = Flask(__name__)
 app.config.from_object(__name__)
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(255))
+    text = db.Column(db.String(255), nullable=False)
+    completed_at = db.Column(db.DateTime)
 
     def __init__(self, text):
         self.text = text
@@ -24,9 +31,11 @@ class Todo(db.Model):
 
 @app.route("/")
 def index():
-    todo_list = Todo.query.all()
+    current_todos = Todo.query.filter(Todo.completed_at == None).all()
+    completed_todos = Todo.query.filter(Todo.completed_at != None).all()
     return render_template("index.html",
-                           todos=todo_list)
+                           todos=current_todos,
+                           completed=completed_todos)
 
 
 @app.route("/add", methods=['POST'])
@@ -43,10 +52,11 @@ def complete():
     ids = request.form.getlist('todo')
     for id in ids:
         todo = Todo.query.get(id)
-        db.session.delete(todo)
+        todo.completed_at = datetime.utcnow()
+        db.session.add(todo)
     db.session.commit()
     return redirect(url_for('index'))
 
 
 if __name__ == "__main__":
-    app.run(debug=app.config['DEBUG'])
+    manager.run()
